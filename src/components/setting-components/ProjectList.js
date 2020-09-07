ee07afca74be7f357a528705423c93e60423963f
ZenVega <50147356+ZenVega@users.noreply.github.com>
2020-09-07 13:03:58 +0200

    implied close by click outside for projectList

--- src/components/setting-components/ProjectList.js
+++ src/components/setting-components/ProjectList.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import './Setting.css'
 import ProjectTag from './ProjectTag'
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
+import {showProjectList} from './../../actions/index'
 
-const ProjectList = (props) => {
-
+const ProjectList = () => {
+  let listRef = useRef(null);
+  const dispatch = useDispatch();
   const projectIDs = useSelector(state => state.data.projects.projectIDs);
   const projects = useSelector(state => state.data.projects);
   const projectSearch = useSelector(state => state.data.projectSearch);
 
   const projectsFiltered = projectIDs.filter(id => projects[id].name.includes(projectSearch));
-  console.log(projectsFiltered)
+
+  useEffect(()=>{
+    const handler = (e)=>{
+      if(!listRef.current.contains(e.target)){
+        dispatch(showProjectList(false))
+      }
+    }
+    document.addEventListener('click', handler);
+    return ()=> document.removeEventListener('click', handler)
+  })
 
   return(
-  <div class="project-list" >
+  <div ref={listRef} className="project-list" >
     {projectsFiltered.map( (id,index) => (
       <ProjectTag 
         key={index}
